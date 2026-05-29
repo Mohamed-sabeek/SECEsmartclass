94284f8f7af0c88283e98999ebd498bf0f418c5d
Mohamed Sabeek <safeesafee0000@gmail.com>
2026-05-29 12:13:24 +0530

    domain added for mail

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -13,7 +13,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async ({ to, subject, html }) => {
   try {
     const { data, error } = await resend.emails.send({
-      from: 'SECE SmartClass <onboarding@resend.dev>',
+      from: 'SECE SmartClass <notifications@projectbro.online>',
       to,
       subject,
       html
